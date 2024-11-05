438ab4e4ee25f4b77d438ae812bf1e9a0659f9ae
luciomelo-77 <luciomelo@ebsc.pt>
2024-11-05 13:41:33 -0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# currency and main goups
+
+Points = 0
+pointsgoup = 1
+
+# upgrades
+
+firstupgrade = false
+
+
+while True
+  if firstupgrade = true
+    pointsgoup *= 2
+    firstupgrade = false
+  else
+
+  
